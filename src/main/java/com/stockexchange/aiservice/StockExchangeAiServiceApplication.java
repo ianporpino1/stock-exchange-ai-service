d4b32dfef541615edd1e4b85765c7e83ccf352e7
ianporpino1 <ianporpino127@gmail.com>
2025-08-20 11:01:43 -0300

    ok

---
+++ src/main/java/com/stockexchange/aiservice/StockExchangeAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stockexchange.aiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchangeAiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockExchangeAiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stockexchange/aiservice/StockExchangeAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockexchange.aiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchangeAiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
